ba7e163207734ef93cfacb1c64e5104332660a62
Andrii Stepura <andriistepura@gmail.com>
2018-12-06 09:31:30 +0100

    Create base

---
+++ leastOccurringValueInArray.rb
@@ -0,0 +1,10 @@
+print "RUBY least occurring value in array \n"
+
+# My function for return least occurring value in array
+def leastOccuringValueInArray arr
+	return false
+end
+
+# Call our function with required array
+arr = ["a", "a", "a", "a", "b", "b", "c", "d", "d", "d"]
+print leastOccuringValueInArray(arr)
